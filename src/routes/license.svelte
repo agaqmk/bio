687bcaaec4b3708200040595b96a0fab55276e4d
YieldingExploiter <yieldingexploiter@users.noreply.github.com>
2022-11-08 02:06:22 +0100

    fix: :bug: no more downloading the license file on accident

--- src/routes/license.svelte
+++ src/routes/license.svelte
@@ -5,7 +5,7 @@
 <Link to="/">Back</Link>
 
 <iframe
-  src="/bio/LICENSE"
+  src="/bio/LICENSE.txt"
   frameborder="0"
   style="width: 100%; height: calc(100% - 3em); min-width: 40vw; min-height: 30vh; border-radius: 8px; filter: invert() hue-rotate(180deg); background: #ffffff; text-align: center;"
   title="agpl"
